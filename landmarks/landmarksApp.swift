ba537cdddf44b9dbda5d78aa3ef5f0cff104a4d9
clerdson <clerdsonj@gmail.com>
2022-04-04 11:10:11 -0300

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by clerdson on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
